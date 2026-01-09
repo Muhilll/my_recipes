f0f3640230398d11f8376c3f8657d8f40869ee02
Muhammad Ilham <ilhamthetamvan@gmail.com>
2026-01-09 08:01:46 +0800

    update dashboard admin

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Resep;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index(){
-        return view('admin.dashboard', ['type_menu' => '']);
+        $jumAdmin = User::where('role', 'admin')->count();
+        $jumUser = User::where('role', 'user')->count();
+        $jumMember = User::where('status', 'member')->count();
+        $jumResep = Resep::all()->count();
+
+        $resepTerbaru = Resep::latest()->take(5)->get();
+        $memberTerbaru = User::where('status', 'member')->latest()->take(5)->get();
+        return view('admin.dashboard', [
+            'jumAdmin' => $jumAdmin,
+            'jumUser' => $jumUser,
+            'jumMember' => $jumMember,
+            'jumResep' => $jumResep,
+            'resepTerbaru' => $resepTerbaru,
+            'memberTerbaru' => $memberTerbaru,
+            'type_menu' => ''
+        ]);
     }
 }
